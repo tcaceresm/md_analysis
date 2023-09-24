9a1010e72efef91be733a78618dafbcb9adbe08a
Tomás Cáceres M <tcaceres@uc.cl>
2023-09-24 19:46:58 -0300

    Deleted some scripts, added tleap_input

--- scripts/main_scripts/copy_files.sh
+++ scripts/main_scripts/copy_files.sh
@@ -19,7 +19,6 @@ Help()
    echo "e     0|1. Process equilibration output."
    echo "p     0|1. Process production output"
    echo "t     0|1. Process topology."
-   echo "m     0|1. Remove WAT from trajectories"
    echo "r     0|1. Compute RMSD from trajectories"
    echo
 }
@@ -28,7 +27,7 @@ Help()
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":h:i:n:d:e:p:t:r:" option; do
+while getopts ":hi:n:d:e:p:t:r:" option; do
    case $option in
       h) # Print this help
          Help
@@ -57,6 +56,7 @@ done
 
 # Ligandos analizados
 declare -a arr=("ben" "bma" "cfa" "cpoa" "cpya" "flu" "iaa" "iaaee" "ipa" "naa" "nta" "paa" "pic" "qui" "tri" "trp" ) 
+declare -a arr=("iaa") 
 
 #Ruta de la carpeta del script (donde se encuentra este script)
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -69,7 +69,7 @@ for LIG in "${arr[@]}"
     do
     echo "Doing for $LIG"
     
-for i in 1 2 3 4 5	
+for i in 1	
     do
 
     EQUI="${WDPATH}/MD_am1/${LIG}${i}/com/equi/"
@@ -87,37 +87,32 @@ for i in 1 2 3 4 5
     then
     
 	    echo "Copying files to $PROD"
-            
-	    echo "Copying process_mdout.perl to ${PROD}"
-            cp $SCRIPT_PATH_prod/process_mdout.perl $PROD	    
 	    
+	    cd $PROD
 
-	    if [[ $rm_hoh -eq 1 ]]
-	    then
-	    	echo "Copying (and overwriting) $RM_HOH"
+	    echo "Copying process_mdout.perl to ${PROD}"
+            cp $SCRIPT_PATH_prod/process_mdout.perl $PROD
+            /usr/bin/perl ${PROD}/process_mdout.perl *.out  
+	    
+  	    if [[ $rmsd -eq 1 ]]
+    	    then
+    	        echo "Copying (and overwriting) $RM_HOH"
 	    	cp $SCRIPT_PATH_prod/$RM_HOH $PROD
 	    	sed -i "s/LIG/${LIG}${i}/g" "$PROD/$RM_HOH"
 	    	sed -i "s/NRES/${N_RES}/g" "$PROD/$RM_HOH"
-	    fi
-	    
-	    if [[ $rm_hoh_mmpbsa -eq 1 ]]
-	    then
-	    	echo "Copying (and overwriting) $RM_HOH_mmpbsa"
-	    	cp $SCRIPT_PATH_prod/$RM_HOH_mmpbsa $PROD
-	    	sed -i "s/LIG/${LIG}${i}/g" "$PROD/$RM_HOH_mmpbsa"
-	    	sed -i "s/NRES/${N_RES}/g" "$PROD/$RM_HOH_mmpbsa"
-	    fi
-	    
-   	    if [[ $rmsd -eq 1 ]]
-    	    then
+	    	
+	    	echo "Removing WAT from trajectories"
+	    	${AMBERHOME}/bin/cpptraj -i ${PROD}/${RM_HOH}
+	    	
             	echo "Copying (and overwriting) $RMSD"
 		cp $SCRIPT_PATH_prod/$RMSD $PROD
 		sed -i "s/LIG/${LIG}${i}/g" "$PROD/$RMSD"
 		sed -i "s/NRES/${N_RES}/g" "$PROD/$RMSD"
+		
+		echo "Calculating RMSD from unsolvated trajectories"
+		${AMBERHOME}/bin/cpptraj -i ${PROD}/${RMSD}
 	    fi
-		    
-	    echo "Copying process_mdout.perl to ../MD_am1/${LIG}/com/prod"
-	    cp $SCRIPT_PATH_prod/process_mdout.perl $PROD
+            cd $SCRIPT_PATH
 
     fi
     
@@ -134,21 +129,30 @@ for i in 1 2 3 4 5
     if [[ $equi -eq 1 ]]
     then
         
-	    echo "Copying files to ../MD_am1/${LIG}${i}/com/equi"
+	    echo "Copying files to $EQUI"
+	    
+	    cd $EQUI
 	    
 	    echo "Copying (and overwriting) $RM_HOH_equi"
-	    cp $SCRIPT_PATH/$RM_HOH_equi $EQUI
+	    cp $SCRIPT_PATH_equi/$RM_HOH_equi $EQUI
 	    sed -i "s/LIGN/${LIG}${i}/g" "$EQUI/$RM_HOH_equi"
 	    sed -i "s/NRES/${N_RES}/g" "$EQUI/$RM_HOH_equi"
+
+	    echo "Removing WAT from trajectories"
+	    ${AMBERHOME}/bin/cpptraj -i ${EQUI}/${RM_HOH_equi}	    
 	    
 	    echo "Copying (and overwriting) $RMSD_equi"
-	    cp $SCRIPT_PATH/$RMSD_equi $EQUI
+	    cp $SCRIPT_PATH_equi/$RMSD_equi $EQUI
 	    sed -i "s/LIGN/${LIG}${i}/g" "$EQUI/$RMSD_equi"
 	    sed -i "s/NRES/${N_RES}/g" "$EQUI/$RMSD_equi"
+	    
 
 	        
 	    echo "Copying (and overwriting) process_mdout.perl"
 	    cp $SCRIPT_PATH/process_mdout.perl $EQUI
+	    /usr/bin/perl $EQUI/process_mdout.perl min_ntr_h.out min_ntr_l.out md_nvt_ntr.out md_npt_ntr.out md_nvt_red_**.out
+	    
+	    cd $SCRIPT_PATH
     fi
 
     done
