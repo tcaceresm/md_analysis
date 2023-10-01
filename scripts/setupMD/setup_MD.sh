5f8d6d7b309c9eb2b349b240ac98c1774934056d
Tomás Cáceres M <tcaceres@uc.cl>
2023-10-01 20:44:26 -0300

    Almost done with setup MD

--- scripts/setupMD/setup_MD.sh
+++ scripts/setupMD/setup_MD.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
-
-#### CHANGE THIS VARIABLES #####
-#!/usr/bin/bash
 set -e
 set -u
 set -o pipefail
@@ -13,16 +10,10 @@ Help()
 {
    # Display Help
 
-   echo "Syntax: copy_files.sh [-h|i|n|d|e|p|t]"
+   echo "Syntax: copy_files.sh [-h|i|n|d]"
    echo "options:"
    echo "h     Print help"
-   echo "i     ID of protein structure."
-   echo "n     Residue Number in structure complex."
    echo "d     Working Directory."
-   echo "e     0|1. Process equilibration output."
-   echo "p     0|1. Process production output"
-   echo "t     0|1. Process topology."
-   echo "r     0|1. Compute RMSD from trajectories"
    echo
 }
 
@@ -30,128 +21,205 @@ Help()
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":hi:n:d:e:p:t:r:" option; do
+while getopts ":hi:d:" option; do
    case $option in
       h) # Print this help
          Help
          exit;;
-      i) # Enter a folder ID.
-         ID=$OPTARG;;
-      n) # Enter the residue Number from PDB complex structure.
-         N_RES=$OPTARG;;
       d) # Enter the MD Directory
-         WDPATH=$OPTARG;;
-      e) # Equilibration processing
-         equi=$OPTARG;;
-      p) # Production processing
-         prod=$OPTARG;;
-      t) # Topology processing
-         topo=$OPTARG;;
-      r) # Compute RMSD
-         rmsd=$OPTARG;;
+         WD_PATH=$OPTARG;;
      \?) # Invalid option
          echo "Error: Invalid option"
          exit;;
    esac
 done
 
+#Ruta de la carpeta del script (donde se encuentra este script)
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#echo "SCRIPT PATH $SCRIPT_PATH"
+
+# Ruta de la carpeta de trabajo. Es SCRIPT_PATH / WD_PATH
+WDPATH=${SCRIPT_PATH}/$WD_PATH
 
 # Ligandos analizados
-declare -a arr=("ben" "bma" "cfa" "cpoa" "cpya" "flu" "iaa" "iaaee" "ipa" "naa" "nta" "paa" "pic" "qui" "tri" "trp" )
-#declare -a arr=("iaa")
+declare -a LIGANDS_MOL2=($(ls ${SCRIPT_PATH}/ligands/))
+declare -a LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
+#echo "LIGANDS ${LIGANDS[*]}"
 
-#Ruta de la carpeta del script (donde se encuentra este script)
-SCRIPT_PATH="/mnt/Backup2/scripts/prepare_calc_mmpbsa"
-#Ruta de las simulaciones
-WDPATH="/mnt/Backup2/${ID}/protocolo_n5_30ns" #ojo con el protocolo
+COFACTOR_MOL2=($(ls ${SCRIPT_PATH}/cofactor/))
+#echo "COFACTOR mol2 $COFACTOR_MOL2"
+COFACTOR=($(sed "s/.mol2//g" <<< "${COFACTOR_MOL2[*]}"))
+#echo "COFACTOR $COFACTOR"
 
+RECEPTOR_PDB=($(ls ${SCRIPT_PATH}/receptor/))
+RECEPTOR=($(sed "s/.pdb//g" <<< "${RECEPTOR_PDB[*]}"))
+# Input para LEaP
+LEAP_SCRIPT_1="leap_topo_vac.in"
+LEAP_SCRIPT_2="leap_topo_solv.in"
+LEAP_SCRIPT_3="leap_create_com.in"
+LEAP_LIB="leap_lib.in"
 
-degron=0 #procesar input para degron mmpbsa. Solo si ya existen las trayectorias
-leap_script="leap_topo_gb0_pb3.in"
-extract_coord="extract_coordinates_com.in"
-mmpbsa_in="mmpbsa.in"
-snapshots="s1_3000_30"
-protocol="pb3_gb0"
-#LAST_ATOM=9279 # el último átomo considerado como receptor
-#TOTAL_ATOM=104595 #Todos los atomos, contando al solvente. Si no sabes, revisa la topologia solvatada del complejo
-
+#echo "RECEPTOR $RECEPTOR"
+echo "
 ##############################
+Welcome to SetupMD v0.0.0
+Author: Tomás Cáceres <caceres.tomas@uc.cl>
+Laboratory of Molecular Design <http://schuellerlab.org/>
+https://github.com/tcaceresm/md_analysis
+Powered by high fat food and procrastination
+##############################
+"
+echo "
+##############################
+Checking existence of MD folder
+##############################
+"
+if test -e "${WDPATH}/MD"
+    then
+        echo "${WDPATH}/MD/ exists"
+        echo "CONTINUE
+        "
+    else
+        echo "${WDPATH}/MD/ do not exists"
+        echo "Creating MD folder at ${WDPATH}"
+        echo "Creating cofactor_lib and receptor folders"
+        mkdir -p ${WDPATH}/MD/{cofactor_lib,receptor}
+        echo "DONE!
+        "
+    fi 
 
-for LIG in "${arr[@]}"
+# Prepare receptor. 
+echo "
+####################################
+Preparing receptor ${RECEPTOR}
+####################################
+"
+RECEPTOR_PATH=$WDPATH/MD/receptor/
+cp $SCRIPT_PATH/receptor/$RECEPTOR_PDB $RECEPTOR_PATH
+$AMBERHOME/bin/pdb4amber -i $WDPATH/MD/receptor/$RECEPTOR_PDB -o $WDPATH/MD/receptor/${RECEPTOR}_prep.pdb --add-missing-atoms --no-conect --nohyd --reduce
+
+echo "Done"
+
+
+
+# Prepare cofactor. 
+echo "
+####################################
+Preparing cofactor ${COFACTOR_MOL2}
+####################################
+"
+
+COFACTOR_LIB=${WDPATH}/MD/cofactor_lib
+
+cp $SCRIPT_PATH/cofactor/$COFACTOR_MOL2 $COFACTOR_LIB
+cp $SCRIPT_PATH/input_files/topo/leap_lib.in $COFACTOR_LIB
+sed -i "s/LIGND/${COFACTOR}/g" $COFACTOR_LIB/leap_lib.in
+sed -i "s/LIG/${COFACTOR}/g" $COFACTOR_LIB/leap_lib.in
+
+cd $COFACTOR_LIB 
+
+echo "Computing net charge from partial charges of mol2 file"
+COFACTOR_NET_CHARGE=$(awk '/ATOM/{ f = 1; next } /BOND/{ f = 0 } f' $COFACTOR_MOL2 | awk '{sum += $9} END {printf "%.0f\n", sum}')
+echo "Net charge of ${COFACTOR_MOL2}: ${COFACTOR_NET_CHARGE}"
+
+
+
+$AMBERHOME/bin/antechamber -i ${COFACTOR_MOL2} -fi mol2 -o ${COFACTOR_MOL2} -fo mol2 -c bcc -nc $COFACTOR_NET_CHARGE -rn $COFACTOR
+$AMBERHOME/bin/parmchk2 -i $COFACTOR_MOL2 -f mol2 -o "${COFACTOR}.frcmod"
+$AMBERHOME/bin/tleap -f ${COFACTOR_LIB}/${LEAP_LIB}
+ 
+cd $SCRIPT_PATH
+   
+echo "Done preparing cofactor"
+    
+
+
+
+for LIG in "${LIGANDS[@]}" #Create folders and copy input files and mol2
     do
     
-    if test -f "${WDPATH}/calc_a_1t/" 
+    echo "Checking existence of MD/${LIG} folder"
+    
+    if test -e ${WDPATH}/MD/${LIG} 
     then
-        echo "${WDPATH}/calc_a_1t/ exist"
-        echo "CONTINUE\n"
+        echo "${WDPATH}/MD/${LIG} exist"
+        echo "CONTINUE
+        "
     else
-     	echo "${WDPATH}/calc_a_1t/ do not exist"
-       	echo "Creating ${WDPATH}/calc_a_1t/"
-	mkdir "${WDPATH}/calc_a_1t/${LIG}/"
-       	echo "DONE\n"
+     	echo "${WDPATH}/MD/${LIG} do not exist"
+       	echo "Creating directiores at ${WDPATH}/MD for ${LIG}"
+        mkdir -p ${WDPATH}/MD/${LIG}/{lib,topo,setupMD/{rep1/{equi,prod},rep2/{equi,prod},rep3/{equi,prod},rep4/{equi,prod},rep5/{equi,prod}}}
+       	echo "DONE
+       	"
     fi   	
     
-    TOPO="${WDPATH}/calc_a_1t/${LIG}/topo/"
-    
-  
-    echo "Doing for ${LIG}"
-    echo "Creating directories"
-    
-    mkdir -p ${WDPATH}/calc_a_1t/${LIG}/{topo,snapshots_rep1,snapshots_rep2,snapshots_rep3,snapshots_rep4,snapshots_rep5,${snapshots}/${protocol}/{rep1,rep2,rep3,rep4,rep5}}
-    
-    echo "Copying files to $TOPO"  
-    echo "Copying ${leap_script} to $TOPO"
-    cp $SCRIPT_PATH/${leap_script} $TOPO
-    sed -i "s/ID/${ID}/g" "${TOPO}/${leap_script}"	  
-    sed -i "s/LIGND/${LIG}1/g" "${TOPO}/${leap_script}"
-    sed -i "s+WDPATH+${WDPATH}+g" "${TOPO}/${leap_script}"
-    sed -i "s+RUTA+${WDPATH}/calc_a_1t/${LIG}/topo+g" "${TOPO}/${leap_script}"
-    sed -i "s+MD_am1+${WDPATH}/MD_am1/${LIG}1/cryst+g" "${TOPO}/${leap_script}"
-    
-# this is to obtain total atom from parmtop file
-   #TOTAL_ATOM=$(cat ${WDPATH}/MD_am1/${LIG}1/cryst/${LIG}1_solv_com.pdb | tail -n 3 | grep 'ATOM' | awk '{print $2}') #por si se usan trajectorias solvatadas para la extraccion
-   TOTAL_ATOM=$(cat ${TOPO}/${LIG}1_vac_com.pdb | tail -n 3 | grep 'ATOM' | awk '{print $2}') # Atomos totales del complejo
-   
-   #LAST_ATOM_REC=$(cat ${WDPATH}/MD_am1/${LIG}1/cryst/${LIG}1_vac_rec.pdb | tail -n 3 | grep 'ATOM' | awk '{print $2}') # last atom del receptor #por si se usan trajectorias solvatadas para la extraccion
-   LAST_ATOM_REC=$(cat ${TOPO}/${LIG}1_vac_rec.pdb | tail -n 3 | grep 'ATOM' | awk '{print $2}') # last atom del receptor   
-   
-   FIRST_ATOM_LIG=$(($LAST_ATOM_REC + 1))
-   LAST_ATOM_LIG=$TOTAL_ATOM
+      
+    TOPO=${WDPATH}/MD/${LIG}/topo
+    LIGAND_LIB=${WDPATH}/MD/${LIG}/lib
 
-for i in 1 2 3 4 5 	
-    do
-
-	
-    SNAP="${WDPATH}/calc_a_1t/${LIG}/snapshots_rep${i}/"
+    echo "Copying files to $TOPO  
+  Copying ${LEAP_SCRIPT_1}, ${LEAP_SCRIPT_2} to $TOPO
+  Copying ${LEAP_LIB} to ${LIGAND_LIB}"
     
-    cp $SCRIPT_PATH/$extract_coord $SNAP
-    #sed -i "s/REP/${i}/g" $SNAP/$extract_coord
-    sed -i "s/LIGND/${LIG}${i}/g" $SNAP/$extract_coord
-    sed -i "s/TOTAL_ATOM/${TOTAL_ATOM}/g" $SNAP/$extract_coord
-    sed -i "s/LAST_ATOM_REC/${LAST_ATOM_REC}/g" $SNAP/$extract_coord
-    sed -i "s/FIRST_ATOM_LIG/${FIRST_ATOM_LIG}/g" $SNAP/$extract_coord     
-    sed -i "s/LAST_ATOM_LIG/${LAST_ATOM_LIG}/g" $SNAP/$extract_coord    
-    sed -i "s+RUTA+${WDPATH}/MD_am1/${LIG}${i}/com/prod/+g" $SNAP/$extract_coord
+    cp $SCRIPT_PATH/input_files/topo/${LEAP_SCRIPT_1} $TOPO #TODO: check if file exists
+    cp $SCRIPT_PATH/input_files/topo/${LEAP_SCRIPT_2} $TOPO # same as above
+    cp $SCRIPT_PATH/input_files/topo/${LEAP_SCRIPT_3} $TOPO
+    cp $SCRIPT_PATH/input_files/topo/${LEAP_LIB} $LIGAND_LIB 
+    cp $SCRIPT_PATH/ligands/${LIG}.mol2 $LIGAND_LIB # copy ligand.mol2 to lib folder
+
+    sed -i "s/LIGND/${LIG}/g" ${TOPO}/${LEAP_SCRIPT_1} $TOPO/$LEAP_SCRIPT_2 ${TOPO}/${LEAP_SCRIPT_3} $LIGAND_LIB/$LEAP_LIB
+    sed -i "s+TOPO_PATH+${TOPO}+g" ${TOPO}/${LEAP_SCRIPT_1} ${TOPO}/${LEAP_SCRIPT_2} ${TOPO}/${LEAP_SCRIPT_3}
+    sed -i "s+COFACTOR_LIB_PATH+${COFACTOR_LIB}+g" ${TOPO}/${LEAP_SCRIPT_1} ${TOPO}/${LEAP_SCRIPT_2} ${TOPO}/${LEAP_SCRIPT_3}
+    sed -i "s/COF/${COFACTOR}/g" ${TOPO}/${LEAP_SCRIPT_1} ${TOPO}/${LEAP_SCRIPT_2} ${TOPO}/${LEAP_SCRIPT_3}
+    sed -i "s+LIGAND_LIB_PATH+${LIGAND_LIB}+g" ${TOPO}/${LEAP_SCRIPT_1} ${TOPO}/${LEAP_SCRIPT_2} ${TOPO}/${LEAP_SCRIPT_3}
+    sed -i "s+REC_PATH+${RECEPTOR_PATH}+g" ${TOPO}/${LEAP_SCRIPT_3}
+    sed -i "s/RECEPTOR/${RECEPTOR}/g" ${TOPO}/${LEAP_SCRIPT_3} 
+echo "
+####################################
+Preparing ligand ${LIG}
+####################################
+"
+    cd $LIGAND_LIB
+    $AMBERHOME/bin/antechamber -i $LIGAND_LIB/$LIG.mol2 -fi mol2 -o $LIGAND_LIB/$LIG.mol2 -fo mol2 -c bcc -rn LIG
+    $AMBERHOME/bin/antechamber -i $LIGAND_LIB/$LIG.mol2 -fi mol2 -o $LIGAND_LIB/${LIG}_lig.pdb -fo pdb -dr n -rn LIG
+    $AMBERHOME/bin/parmchk2 -i $LIGAND_LIB/$LIG.mol2 -f mol2 -o "$LIGAND_LIB/$LIG.frcmod"
+    $AMBERHOME/bin/tleap -f $LIGAND_LIB/$LEAP_LIB
+    cd $SCRIPT_PATH
     
-    MMPBSA="${WDPATH}/calc_a_1t/${LIG}/${snapshots}/${protocol}/rep${i}/"
-    cp "$SCRIPT_PATH/run_mmpbsa_lig2.sh" $MMPBSA
-    sed -i "s/LIG/${LIG}/g" "$MMPBSA/run_mmpbsa_lig2.sh"
-    sed -i "s/repN/rep${i}/g" "$MMPBSA/run_mmpbsa_lig2.sh"
-    sed -i "s/REP/${i}/g" "$MMPBSA/run_mmpbsa_lig2.sh"
-    
-    cp "$SCRIPT_PATH/run_mmpbsa_slurm2.sh" $MMPBSA
-    sed -i "s/LIG/${LIG}/g" "$MMPBSA/run_mmpbsa_slurm2.sh"
-    sed -i "s/repN/rep${i}/g" "$MMPBSA/run_mmpbsa_slurm2.sh"
-    sed -i "s/REP/${i}/g" "$MMPBSA/run_mmpbsa_slurm2.sh"
+echo "
+Done preparing ligand
+"
 
+echo "    
+####################################
+Preparing Complex $RECEPTOR $LIG
+####################################
+"
+     $AMBERHOME/bin/tleap -f $TOPO/${LEAP_SCRIPT_3} # Obtain complex.pdb
+#    $AMBERHOME/bin/tleap -f $TOPO/${LEAP_SCRIPT_1} # Obtain vacuum
+#    $AMBERHOME/bin/tleap -f $TOPO/${LEAP_SCRIPT_2} # solvated
 
+echo "
+Done preparing complex $RECEPTOR $LIG
+" 
+echo "    
+####################################
+Preparing MD files
+####################################
+"
+    for rep in 1 2 3 4 5
+        do
+        TOTALRES=$(cat ${TOPO}/${LIG}_com.pdb | tail -n 3 | grep 'ATOM' | awk '{print $5}') # last atom del receptor
+        
+        cp $SCRIPT_PATH/input_files/equi/*  $WDPATH/MD/$LIG/setupMD/rep$rep/equi/
+        sed -i "s/RES_TOTAL/${TOTALRES}/g" $WDPATH/MD/$LIG/setupMD/rep$rep/equi/*
+        
+        cp $SCRIPT_PATH/input_files/prod/md_prod.in $WDPATH/MD/$LIG/setupMD/rep$rep/prod/
+        
+        done
+     echo "Done copying files for MD
+        "
 
-    cp "$SCRIPT_PATH/$mmpbsa_in" $MMPBSA
-    sed -i "s/LIGND_REP/${LIG}${i}/g" $MMPBSA/${mmpbsa_in}
-    sed -i "s/LIGND/${LIG}/g" $MMPBSA/${mmpbsa_in}
-    
- 
-
-    done
-done
 echo "DONE!"
+done
+
