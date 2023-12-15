da1a9a6c9074cd7297a2723ac7c4f56905cada66
Tomás Cáceres M <caceres.tomas@uc.cl>
2023-12-15 03:13:40 -0300

    updated scritp

--- scripts/mmpbsa/setup_MMPBSA.sh
+++ scripts/mmpbsa/setup_MMPBSA.sh
@@ -148,9 +148,9 @@ Going to extract coordinates starting at ${START}, ending at ${END} by offset ${
     sed -i "s/LIG/${LIG}/g" "$MMPBSA/run_mmpbsa_lig.sh"
     sed -i "s/repN/rep${i}/g" "$MMPBSA/run_mmpbsa_lig.sh"
     sed -i "s/MMPBSA_IN/${mmpbsa_in}/g" "$MMPBSA/run_mmpbsa_lig.sh"
-    sed -i "s+MD_TOPO+${TOPO_MD}+g" "$MMPBSA/run_mmpbsa_lig.sh"
-    sed -i "s+MMPBSA_SNAPS+${SNAP}+g" "$MMPBSA/run_mmpbsa_lig.sh"
-    sed -i "s+MMPBSA_PATH+${MMPBSA}+g" "$MMPBSA/run_mmpbsa_lig.sh"
+    sed -i "s+MD_TOPO+~/2p1q/${LIG}/topo+g" "$MMPBSA/run_mmpbsa_lig.sh"
+    sed -i "s+MMPBSA_SNAPS+~/2p1q/MMPBSA/${LIG}_gbind/snapshots_rep${i}+g" "$MMPBSA/run_mmpbsa_lig.sh"
+    sed -i "s+MMPBSA_PATH+~/2p1q/MMPBSA/${LIG}_gbind/s1_3000_30/pb4_gb1/rep${i}/+g" "$MMPBSA/run_mmpbsa_lig.sh"
     sed -i "s/METHOD/${method}/g" "$MMPBSA/run_mmpbsa_lig.sh"
     sed -i "s+MMPBSA_TMP_PATH+${WDPATH}/MMPBSA/tmp/+g" "$MMPBSA/run_mmpbsa_lig.sh"
     
