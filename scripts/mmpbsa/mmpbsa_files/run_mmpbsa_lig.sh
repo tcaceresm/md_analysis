41864d06fe7779a28b7a8d9faf11696702da5716
Tomás Cáceres M <caceres.tomas@uc.cl>
2023-12-15 01:04:27 -0300

    added scripts

---
+++ scripts/mmpbsa/mmpbsa_files/run_mmpbsa_lig.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SCRIPT=/home/tcaceres/calc_a_1t/LIG/s1_3000_30/pb3_gb0/run_mmpbsa.pbs
+SY=LIG
+CALC=LIG_repN_METHOD
+PARAMS=MMPBSA_IN
+TOPO=MD_TOPO
+SNAPS=MMPBSA_SNAPS
+
+
+#
+WD_PATH=MMPBSA_PATH
+cd $WD_PATH
+
+#
+# --- Create tmp directory
+#
+mkdir MMPBSA_TMP_PATH
+cd MMPBSA_TMP_PATH
+mkdir $CALC
+cd $CALC
+cp $WD_PATH/$PARAMS .
+ln -s $TOPO topo
+ln -s $SNAPS snapshots
+
+#
+# --- Execute
+#
+#MMPBSA=$AMBERHOME/bin/mm_pbsa.pl
+perl /home/tcaceres/.conda/envs/ambertools22.5/bin/mm_pbsa.pl $PARAMS
+
+#
+# --- Zipping output
+#
+gzip -9 ${SY}*
+cp ${SY}* $WD_PATH
+#rm -r /home/tcaceres/afb5_noDegron/calc_a_1t/tmp/$CALC
+gunzip $WD_PATH/${SY}_statistics.out
+
