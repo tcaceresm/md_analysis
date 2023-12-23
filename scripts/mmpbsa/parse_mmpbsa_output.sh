32f6599548b7f84bb8294ff1238205636aa3575f
Tomás Cáceres M <caceres.tomas@uc.cl>
2023-12-23 20:05:49 -0300

    updated script

--- scripts/mmpbsa/parse_mmpbsa_output.sh
+++ scripts/mmpbsa/parse_mmpbsa_output.sh
@@ -59,20 +59,25 @@ for i in 1 2 3 4 5
     do
         for LIG in "${LIGANDS[@]}"
     	do
-    	echo "DOING FOR ${LIG}${i}"
-    	if [ "$DEGRON" -eq 1 ]
-    	then
-        OUT="${WDPATH}/MMPBSA/${LIG}_degron_gbind/${SNAPSHOTS}/${METHOD}/rep${i}/"   	
-    	else    	
-        OUT="${WDPATH}/MMPBSA/${LIG}_gbind/${SNAPSHOTS}/${METHOD}/rep${i}/"
+    	    echo "DOING FOR ${LIG}${i}"
+    	    if [ "$DEGRON" -eq 1 ]
+    	    then
+                OUT="${WDPATH}/MMPBSA/${LIG}_degron_gbind/${SNAPSHOTS}/${METHOD}/rep${i}/"   	
+    	    else    	
+                OUT="${WDPATH}/MMPBSA/${LIG}_gbind/${SNAPSHOTS}/${METHOD}/rep${i}/"
         
-    	fi
-            	if [ "$DECOMP" -eq 1 ]
-            	then
-                	
+    	    fi
+            if [ "$DECOMP" -eq 1 ]
+            then  	
 		grep -i 'delta' ${OUT}/${LIG}_statistics_decomp.out -A9999 | grep -iv 'delta' | grep -iv 'number' > ${OUT}/${LIG}_statistics_parsed.out || continue
-		else
+	    else
+	    
+	    if [ "$DEGRON" -eq 1 ]
+	    then
+	        grep -i 'delta' ${OUT}/${LIG}_degron${i}_statistics.out.snap -A9999 | grep -iv 'delta' | grep -iv 'number' > ${OUT}/${LIG}_statistics_snap_parsed.out || continue
+	    
+	    else
 		grep -i 'delta' ${OUT}/${LIG}_statistics.out.snap -A9999 | grep -iv 'delta' | grep -iv 'number' > ${OUT}/${LIG}_statistics_snap_parsed.out || continue
-	        fi
+	    fi
     	done
     done
