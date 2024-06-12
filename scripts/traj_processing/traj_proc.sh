3bd1af6c6ba5aa222ed7607c5e18ba323dd97443
tcaceresm <caceres.tomas99@gmail.com>
2024-06-12 16:50:02 -0400

    updated file

--- scripts/traj_processing/traj_proc.sh
+++ scripts/traj_processing/traj_proc.sh
@@ -173,8 +173,7 @@ for LIG in "${LIGANDS[@]}"
                   ### REMOVE HOH
                   if [[ $WAT -eq 1 ]]
                      then
-                        cp ${EQUI_FILES}/${RM_HOH_equi} $EQUI/npt/
-                        PrepareInputFile $EQUI $EQUI_FILES $EQUI/npt/$RM_HOH_equi $LIG $N_RES $TOPO
+                        PrepareInputFile $EQUI/npt/ $EQUI_FILES $EQUI/npt/$RM_HOH_equi $LIG $N_RES $TOPO
                         ${AMBERHOME}/bin/cpptraj -i ${EQUI}/npt/${RM_HOH_equi}
                   fi
 
