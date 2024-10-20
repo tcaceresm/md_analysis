e00ef006327dcb4cc97fa2b63a051e78a3ad33da
tcaceresm <caceres.tomas99@gmail.com>
2024-10-20 19:09:56 -0300

    updated file

--- scripts/traj_processing/traj_proc.sh
+++ scripts/traj_processing/traj_proc.sh
@@ -9,65 +9,251 @@ set -o pipefail
 function Help
 {
    # Display Help
-
-   echo "Syntax: traj_proc.sh OPTIONS"
-   echo "options:"
-   echo "h     Print help"
-   echo "d     Working Directory."
-   echo "e     0|1. Process equilibration output."
-   echo "p     0|1. Process production output"
-   echo "r     0|1. Compute RMSD from trajectories"
-   echo "W     0|1. Remove WAT from trajectories"
+   echo "Usage: bash traj_proc.sh [-h] [-d DIRECTORY] [-n REPLICAS] [-e 0|1] [-p 0|1] [-r 0|1] [-w 0|1] [-o 0|1]"
    echo
+   echo "This script process MD trajectories."
+   echo "You must run run_MD.sh first, using the same working directory."    
+   echo
+   echo "Options:"
+   echo "  -h     Print help"
+   echo "  -d     Working Directory."
+   echo "  -n     Number of repetitions."
+   echo "  -k     Process Protein-only MD."
+   echo "  -z     Process Protein-Ligand MD."
+   echo "  -e     0|1. (default=0) Process equilibration output."
+   echo "  -p     0|1. (default=0) Process production output"
+   echo "  -r     0|1. (default=0) Compute RMSD from trajectories"
+   echo "  -w     0|1. (default=0) Remove WAT from trajectories"
+   echo "  -o     0|1. (default=0) Process .out files"
 }
 
-################################################################
-# Prepare Input Files. #
-################################################################
-
-function PrepareInputFile
-{
-   #               1       2         3      4    5       6
-   #removeWater $PROD $PROD_FILES $RM_HOH $LIG $N_RES $TOPO
-
-   echo "Copying input $3 file"
-      
-   cp $2/$3 $1
-
-   sed -i "s/LIG/${4}/g" "$1/$3"
-   sed -i "s/NRES/${5}/g" "$1/$3"
-   sed -i "s+TOPO_PATH+${6}+g" "$1/$3"
-
-   cd $1
-
-   #${AMBERHOME}/bin/cpptraj -i ${1}/${3}
-}
-
+# Default values
+PROCESS_EQUI=0
+PROCESS_PROD=0
+PROCESS_RMSD=0
+PROCESS_WAT=0
+PROCESS_OUT_FILES=0
 
 ############################################################
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":hd:e:p:r:w:" option; do
+while getopts ":hd:n:k:z:e:p:r:w:o:" option; do
    case $option in
       h) # Print this help
          Help
          exit;;
       d) # Enter the MD Directory
          WDPATH=$OPTARG;;
+      n) # Replicas
+         N=$OPTARG;;
+      k) # Protein-only
+         PROCESS_ONLY_PROTEIN=$OPTARG;;
+      z) # Protein-ligand
+         PROCESS_PROTEIN_LIGAND=$OPTARG;;
       e) # Equilibration processing
-         equi=$OPTARG;;
+         PROCESS_EQUI=$OPTARG;;
       p) # Production processing
-         prod=$OPTARG;;
+         PROCESS_PROD=$OPTARG;;
       r) # Compute RMSD
-         rmsd=$OPTARG;;
+         PROCESS_RMSD=$OPTARG;;
       w) # Remove waters?
-         WAT=$OPTARG;;
+         PROCESS_WAT=$OPTARG;;
+      o) # Process out files
+         PROCESS_OUT_FILES=$OPTARG;;
      \?) # Invalid option
          echo "Error: Invalid option"
          exit;;
    esac
 done
+
+################################################################
+# Display message                                              #
+################################################################
+
+function displayHello
+{
+
+echo "
+##############################################################
+# Welcome to trajectory processing v0.0.0                    #   
+# Author: Tomás Cáceres <caceres.tomas@uc.cl>                #
+# Laboratory of Molecular Design <http://schuellerlab.org/>  #
+# https://github.com/tcaceresm/md_analysis                   #
+##############################################################
+"
+}
+
+################################################################
+# Prepare input files                                          #
+################################################################
+
+function PrepareInputFile
+{
+
+   local OUTPUT_PATH=$1
+   local INPUT_FILE_PATH=$2
+   local INPUT_FILE=$3
+   local REC_LIG_NAME=$4
+   local NRES=$5
+
+   echo "Copying input $INPUT_FILE file"
+      
+   cp $INPUT_FILE_PATH/$INPUT_FILE $OUTPUT_PATH
+
+   sed -i "s/LIG\|RECEPTOR/${REC_LIG_NAME}/g" "$OUTPUT_PATH/$INPUT_FILE"
+   sed -i "s/NRES/${NRES}/g" "$OUTPUT_PATH/$INPUT_FILE"
+
+}
+
+################################################################
+# Prepare paths & Number of residues                           #
+################################################################
+function obtainPaths
+{
+   local WDPATH=$1
+   local RECEPTOR=$2
+   local PROCESS_ONLY_PROTEIN=$3
+   local PROCESS_PROTEIN_LIGAND=$4
+   local LIG=$5
+
+   if [[ $PROCESS_ONLY_PROTEIN -eq 1 ]]
+   then
+      EQUI_PATH="${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${i}/equi/"
+      PROD_PATH="${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${i}/prod/"
+      TOPO_PATH="${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/topo/"
+      N_RES=$(cat ${TOPO_PATH}/${RECEPTOR}_rec.pdb | tail -n 3 | awk '{print $5}')      
+   fi
+
+   if [[ $PROCESS_PROTEIN_LIGAND -eq 1 ]]
+   then
+      EQUI_PATH="${WDPATH}/MD/${RECEPTOR}/${LIG}/setupMD/rep${i}/equi/"
+      PROD_PATH="${WDPATH}/MD/${RECEPTOR}/${LIG}/setupMD/rep${i}/prod/"
+      TOPO_PATH="${WDPATH}/MD/${RECEPTOR}/${LIG}/topo/"
+      N_RES=$(cat ${TOPO}/${LIG}_com.pdb | tail -n 3 | awk '{print $5}')
+   fi
+}
+
+################################################################
+# Process production .out files                                #
+################################################################
+
+function processProdOutFiles
+{
+   local PROD=$1
+   local PROD_FILES=$2
+
+   echo "Copying process_mdout.perl to ${PROD}"   
+   cp $PROD_FILES/process_mdout.perl $PROD               
+   /usr/bin/perl ${PROD}/process_mdout.perl ${PROD}/*.out
+
+}
+
+################################################################
+# Process equilibration .out files                             #
+################################################################
+
+function processEquiOutFiles
+{
+   local EQUI_PATH=$1
+   local EQUI_FILES=$2
+
+   echo "Copying process_mdout.perl to ${EQUI}"
+   cp $EQUI_FILES/process_mdout.perl $EQUI
+   /usr/bin/perl $EQUI/process_mdout.perl min_ntr_h.out min_ntr_l.out md_nvt_ntr.out md_npt_ntr.out ./$ensemble/*.out
+}
+
+################################################################
+# Process everything                                           #
+################################################################
+
+
+function process_trajectories
+{
+   #echo "PROCESS TRAJECTORIES ARGUMENTS ${@}"
+   local WDPATH=$1
+   local RECEPTOR=$2
+   local LIG=$3
+   local PROCESS_EQUI=$4
+   local PROCESS_PROD=$5
+   local PROCESS_ONLY_PROTEIN=$6
+   local PROCESS_PROTEIN_LIGAND=$7
+   local PROCESS_OUT_FILES=$8
+   local PROCESS_WAT=$9
+   local PROCESS_RMSD=${10}
+
+   obtainPaths ${WDPATH} ${RECEPTOR} ${PROCESS_ONLY_PROTEIN} ${PROCESS_PROTEIN_LIGAND} ${LIG}
+
+   if [[ $PROCESS_PROD -eq 1 ]]
+      then
+         echo ""
+         echo "   ################################"
+         echo "   # Processing Production Files  #"
+         echo "   ################################"
+         echo ""
+
+         if [[ ${PROCESS_OUT_FILES} -eq 1 ]] # Process .out files
+            then
+               cd ${PROD_PATH}
+               processProdOutFiles ${PROD_PATH} ${PROD_INPUT_FILES}
+         fi
+                              
+         if [[ ${PROCESS_WAT} -eq 1 ]] # Remove waters
+            then
+               cd ${PROD_PATH}  
+               PrepareInputFile ${PROD_PATH} ${PROD_INPUT_FILES} ${RM_HOH} ${RECEPTOR} ${N_RES} 
+               ${AMBERHOME}/bin/cpptraj -i ${PROD}/${RM_HOH}
+            else
+               echo "   Not removing WAT from trajectories"
+         fi
+
+         if [[ ${PROCESS_RMSD} -eq 1 ]] #Calculate RMSD
+            then
+               echo "   Computing RMSD"
+               PrepareInputFile ${PROD_PATH} ${PROD_INPUT_FILES} ${RMSD} ${RECEPTOR} ${N_RES}
+               ${AMBERHOME}/bin/cpptraj -i ${PROD_PATH}/${RMSD}
+            else
+               echo "   Not calculating RMSD"
+         fi
+   fi 
+
+   if [[ $PROCESS_EQUI -eq 1 ]] # Process equilibration phase files
+      then
+         echo ""
+         echo "   ##################################"
+         echo "   # Processing Equilibration Files #"
+         echo "   ##################################"
+         echo ""          
+         if [[ ${PROCESS_OUT_FILES} -eq 1 ]]
+            then
+               cd ${EQUI_PATH}
+               processEquiOutFiles ${EQUI_PATH} ${EQUI_INPUT_FILES}
+         fi
+         
+         ### REMOVE HOH
+         if [[ ${PROCESS_WAT} -eq 1 ]]
+            then 
+               cd ${EQUI_PATH}/${ensemble}
+               PrepareInputFile ${EQUI_PATH}/${ensemble} ${EQUI_INPUT_FILES} ${RM_HOH_equi} ${RECEPTOR} ${N_RES} #${TOPO}
+               ${AMBERHOME}/bin/cpptraj -i ${EQUI_PATH}/${ensemble}/${RM_HOH_equi}
+            else
+               echo "   Not removing WAT from trajectories"
+         fi
+
+         ### Calculate RMSD
+         if [[ ${PROCESS_RMSD} -eq 1 ]] #unsolvated coordinates
+            then
+               PrepareInputFile ${EQUI_PATH}/${ensemble} ${EQUI_INPUT_FILES} ${RMSD_equi} ${RECEPTOR} ${N_RES} #${TOPO}
+               echo "   Calculating RMSD"
+               cd ${EQUI_PATH}/${ensemble}
+               ${AMBERHOME}/bin/cpptraj -i ${EQUI_PATH}/${ensemble}/${RMSD_equi}
+            else
+               echo "   Not calculating RMSD"
+               echo ""
+         fi
+         
+   fi
+}
 
 ############################################################
 #                       Main                               #
@@ -75,154 +261,50 @@ done
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-PROD_FILES=$SCRIPT_PATH/prod_processing/
-EQUI_FILES=$SCRIPT_PATH/equi_processing/
+PROD_INPUT_FILES=$SCRIPT_PATH/prod_processing/onlyProtein/
+EQUI_INPUT_FILES=$SCRIPT_PATH/equi_processing/onlyProtein/
 
 WDPATH=$(realpath $WDPATH) #Working directory, where setupMD was configured
 
-# Analyzed ligands
-declare -a LIGANDS_MOL2=($(ls $WDPATH/ligands/))
-declare -a LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
-
+# Analyzed receptor
 RECEPTOR_PDB=($(ls ${WDPATH}/receptor/))
 RECEPTOR=($(sed "s/.pdb//g" <<< "${RECEPTOR_PDB[*]}"))
 
-echo "
-##############################
-Welcome to trajectory processing v0.0.0
-Author: Tomás Cáceres <caceres.tomas@uc.cl>
-Laboratory of Molecular Design <http://schuellerlab.org/>
-https://github.com/tcaceresm/md_analysis
-##############################
-"
+# Input files names
+RM_HOH="remove_hoh_prod.in" 
+RM_HOH_equi="remove_hoh_equi.in" 
 
-for LIG in "${LIGANDS[@]}"
-   do
-      echo "Doing for $LIG"
-    
-      for i in 1 2 3 4 5
+RMSD="prod_rmsd.in"
+RMSD_equi="equi_rmsd.in"
+
+# MD ensemble
+ensemble="npt"
+
+displayHello
+
+for i in $(seq 1 $N)
+do
+   if [[ ${PROCESS_ONLY_PROTEIN} -eq 1 ]]
+      then
+      echo " # Processing Only-protein # "
+      LIG=false
+      process_trajectories ${WDPATH} ${RECEPTOR} ${LIG} ${PROCESS_EQUI} ${PROCESS_PROD} \
+                           ${PROCESS_ONLY_PROTEIN} ${PROCESS_PROTEIN_LIGAND} \
+                           ${PROCESS_OUT_FILES} ${PROCESS_WAT} ${PROCESS_RMSD}
+   fi
+   if [[ ${PROCESS_PROTEIN_LIGAND} -eq 1 ]]
+      then
+      echo " # Processing Protein-Ligand # "
+      # Analyzed ligands
+      declare -a LIGANDS_MOL2=($(ls ${WDPATH}/ligands/))
+      declare -a LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
+
+      for LIG in "${LIGANDS[@]}"
          do
-            echo "
-            ########################
-            # Repetition number $i #
-            ########################            
-            "
-            EQUI="${WDPATH}/MD/${RECEPTOR}/${LIG}/setupMD/rep${i}/equi/"
-            PROD="${WDPATH}/MD/${RECEPTOR}/${LIG}/setupMD/rep${i}/prod/"
-            TOPO="${WDPATH}/MD/${RECEPTOR}/${LIG}/topo/"
-            N_RES=$(cat ${TOPO}/${LIG}_com.pdb | grep "LIG" | tail -n 1 | awk '{print $5}')
-
-            RM_HOH="remove_hoh_prod" #remove_hoh_prod
-            RM_HOH_mmpbsa="remove_hoh_mmpbsa" #remove_hoh_mmpbsa
-            RM_HOH_equi="remove_hoh_equi" #remove_hoh_equi
-            
-            RMSD="prod_rmsd"
-            RMSD_equi="equi_rmsd"
-
-            if [[ $equi -eq 1 ]]
-               then
-                  echo "
-                  #############################################
-                  # Processing Equilibration Files $LIG $i    #
-                  #############################################
-                  "               
-                  cd $EQUI
-
-                  cp $EQUI_FILES/process_mdout.perl $EQUI
-                  echo "Processing *.out files with process_mdout.perl"
-
-                  /usr/bin/perl $EQUI/process_mdout.perl min_ntr_h.out min_ntr_l.out md_nvt_ntr.out md_npt_ntr.out ./npt/*.out
-               
-                  ### REMOVE HOH
-                  if [[ $WAT -eq 1 ]]
-                     then
-                        echo "
-                        #################################################
-                        # Removing WAT from trajectories (equi) $LIG $i #
-                        #################################################
-                        "                     
-                        PrepareInputFile $EQUI/npt/ $EQUI_FILES $RM_HOH_equi $LIG $N_RES $TOPO
-                        ${AMBERHOME}/bin/cpptraj -i ${EQUI}/npt/${RM_HOH_equi}
-                     else
-                        echo "
-                        #####################################################
-                        # Not removing WAT from trajectories (equi) $LIG $i #
-                        #####################################################
-                        "
-                  fi
-
-                  ### Calculate RMSD
-                  if  test -f ${EQUI}/npt/${LIG}_equi.nc && [[ $rmsd -eq 1 ]] #unsolvated coordinates
-                     then
-                        echo "
-                        ##############################################
-                        # Correct unsolvated coordinates available!  #
-                        ##############################################
-                        "
-                        PrepareInputFile $EQUI/npt/ $EQUI_FILES $RMSD_equi $LIG $N_RES $TOPO
-                        echo "Calculating RMSD from unsolvated trajectories"
-                        ${AMBERHOME}/bin/cpptraj -i ${EQUI}/npt/${RMSD_equi}
-                     else
-                        echo "No unsolvated coordinates available. Can't calculate RMSD"
-                  fi
-                
-            fi
-         
-            if [[ $prod -eq 1 ]]
-               then
-                  echo "
-                  #########################################
-                  # Processing Production Files $LIG $i   #
-                  #########################################
-                  "   
-                  cp $PROD_FILES/process_mdout.perl $PROD
-
-                  cd $PROD
-                     
-                  #/usr/bin/perl ${PROD}/process_mdout.perl *.out
-                                   
-               if [[ $WAT -eq 1 ]]
-                  then
-                     echo "
-                     ##########################################
-                     # Removing WAT from trajectories $LIG $i #
-                     ##########################################
-                     "
-                     PrepareInputFile ${PROD} ${PROD_FILES} ${RM_HOH} ${LIG} ${N_RES} ${TOPO}
-                     ${AMBERHOME}/bin/cpptraj -i ${PROD}/${RM_HOH}
-                  else
-                     echo "
-                     ##############################################
-                     # Not removing WAT from trajectories $LIG $i #
-                     ##############################################
-                     "
-               fi
-
-               if [[ $rmsd -eq 1 ]]
-                  then
-                     if test -f ${PROD}/${LIG}_prod_noWAT.nc
-                        then
-                           echo "
-                           #########################################################
-                           # Correct unsolvated production coordinates available!  #
-                           # Calculating RMSD from unsolvated coordinates $LIG $i  #
-                           #########################################################
-                           "
-
-                           PrepareInputFile ${PROD} ${PROD_FILES} ${RMSD} ${LIG} ${N_RES} ${TOPO}
-                           ${AMBERHOME}/bin/cpptraj -i ${PROD}/${RMSD}
-                        else
-                           echo "
-                           ####################################################
-                           # No unsolvated production coordinates available.  #
-                           # Can't calculate RMSD $LIG $i                     #
-                           ####################################################
-                           "
-                     fi
-               fi
-            fi 
-        
-         done
-   done
- 
-echo "DONE!"
+         echo "Ligand is ${LIG}"
+         process_trajectories ${WDPATH} ${RECEPTOR} ${LIG} ${PROCESS_EQUI} ${PROCESS_PROD} \
+                              ${PROCESS_ONLY_PROTEIN} ${PROCESS_PROTEIN_LIGAND} \ 
+                              ${PROCESS_OUT_FILES} ${PROCESS_WAT} ${PROCESS_RMSD}
+      done
+   fi
+done
