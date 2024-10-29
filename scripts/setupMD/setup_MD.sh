ab8a99cd5c55e4c794c4c29f99ac218f9f6f12dd
tcaceresm <caceres.tomas99@gmail.com>
2024-10-29 00:01:38 -0300

    updated file

--- scripts/setupMD/setup_MD.sh
+++ scripts/setupMD/setup_MD.sh
@@ -390,37 +390,6 @@ PrepareProteinLigandMMPGBSAMinimization() {
     do 
         cp ${SCRIPT_PATH}/input_files/mmpgbsa_rescoring/*.in ${MD_FOLDER}/rep${rep}/mmpbgsa_rescoring
         sed -i "s/TOTALRES/${TOTALRES}/g" ${MD_FOLDER}/rep${rep}/mmpbgsa_rescoring/*.in
-
-cat > ${MD_FOLDER}/rep${rep}/mmpbgsa_rescoring/mm_pbsa.in  <<-EOF
-    Input file for PBSA
-    &general
-    startframe=1, endframe=1100, interval=100,
-    verbose=2, keep_files=0, netcdf=1,
-    use_sander=1
-
-    /
-    &pb
-    inp=2, indi=1.0, exdi=80.0, cavity_offset=-0.5692,
-    cavity_surften=0.0378, prbrad=1.4,
-    scale=2.0, linit=1000, istrng=0.0, radiopt=0, 
-    istrng=0.15, fillratio=4.0,
-EOF
-
-function create_mmgbsa_input () {
-
-cat > mm_gbsa.in  <<EOF
-  Input file for GBSA
-&general
-startframe=1, endframe=1,
-verbose=2, keep_files=0, netcdf=1,
-use_sander=1
-/
-&gb
-igb=1, saltcon=0.1, surfoff=0.0,
-surften=0.0072, molsurf=0, probe=0
-EOF
-
-}
     done
 
     echo " Done!"
