f01c4ac394325fd0b88e08667c502a42363279bd
tcaceresm <caceres.tomas99@gmail.com>
2024-11-08 12:03:58 -0300

    gnu parallel version of this script

---
+++ scripts/mmpbsa/mmpbsa_py/run_MMPBSA_parallel.sh
@@ -0,0 +1,289 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Usage: bash run_MMPBSA.sh [-h] [-d DIRECTORY] [-n REPLICAS] [-r 0|1] [-y 0|1] [-n 0|1]"
+   echo
+   echo "This script perform MM/PB(G)SA rescoring of docked poses."
+   echo "You must run setup_MD.sh first, using the same working directory."
+   echo "Minimization needs to be performed at least one time." 
+   echo
+   echo "Options:"
+   echo "  -h                   Print this help"
+   echo "  -d DIRECTORY         Working Directory."
+   echo "  -m MINIMIZATION      (default=0) Run minimization"
+   echo "  -r 0|1               (default=0) Run MM/PBSA rescoring."
+   echo "  -g 0|1               (default=0) Run MM/GBSA rescoring."
+   echo "  -y REPLICAS_START    (default=1). See example below."
+   echo "  -n REPLICAS_END      Replicas. See example below."
+   echo "  -c NUM_CORES         Number of threads to use."
+   echo
+
+}
+
+# Default values
+RUN_MMPBSA=0
+RUN_MMGBSA=0
+REPLICAS_START=1
+RUN_MINIMIZATION=0
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":hd:d:m:r:g:y:n:" option; do
+   case $option in
+      h) # Print this help
+         Help
+         exit;;
+      d) # Enter the MD Directory
+         WD_PATH=$OPTARG;;
+      m) # Run minimization
+         RUN_MINIMIZATION=$OPTARG;;
+      r) # Run mm/pbsa rescoring
+         RUN_MMPBSA=$OPTARG;;
+      g) # Run mm/gbsa rescoring
+         RUN_MMGBSA=$OPTARG;;
+      n) # Replicas
+         REPLICAS_END=$OPTARG;;
+      y) # Replicas start
+         REPLICAS_START=$OPTARG;;
+      c) # Number of threads to use
+         NUM_CORES=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+function displayHello() 
+{
+echo "
+##############################
+Welcome to run_MMPBSA v0.0.0
+Author: Tomás Cáceres <caceres.tomas@uc.cl>
+Laboratory of Molecular Design <http://schuellerlab.org/>
+https://github.com/tcaceresm/md_analysis
+Powered by high fat food and procrastination
+##############################
+"
+}
+
+############################################################
+# Check output
+############################################################
+function check_file() {
+  local FILE=$1
+  
+  if [[ -f "${FILE}.nc" && ! -f "${FILE}_successful.tmp" ]]
+  then
+    echo "${FILE} output exists but didn't finished correctly".
+    echo "Please check ${FILE}.out"
+    echo "Exiting"
+    echo 0
+
+  elif [[ -f "${FILE}_successful.tmp" ]]
+  then
+    echo "${FILE} already executed succesfully."
+    echo "Skipping."
+    echo 1
+  else
+    echo 2
+  fi
+}
+
+############################################################
+# Run minimization
+############################################################
+
+function run_minimization ()
+{
+  # 3 steps minimization in explicit solvent
+  local TOPO=$1
+  local REF=$2
+  local CUDA_EXE=$3
+  echo "####################"
+  echo " Minimization"
+  echo "####################"
+
+  echo "Running min_ntr_h.in"
+  status=$(check_file "min_ntr_h")
+  #status=$? #save output from last command
+
+  if [[ $status -eq 2 ]]
+  then
+    $CUDA_EXE -O -i min_ntr_h.in -o min_ntr_h.out -x min_ntr_h.nc -r min_ntr_h.rst7 -inf min_ntr_h.info -p $TOPO -c $REF.rst7 -ref $REF.rst7
+    touch min_ntr_h_successful.tmp
+  fi
+
+  echo
+  echo "Running min_ntr_l.in"
+  
+  status=$(check_file "min_ntr_l")
+  #status=$?
+  
+  if [[ $status -eq 2 ]]
+  then
+    $CUDA_EXE -O -i min_ntr_l.in -o min_ntr_l.out -x min_ntr_l.nc -r min_ntr_l.rst7 -inf min_ntr_l.info -p $TOPO -c min_ntr_h.rst7 -ref $REF.rst7
+    touch min_ntr_l_successful.tmp
+  fi
+
+  echo
+  echo "Running min_no_ntr.in"
+  
+  status=$(check_file "min_no_ntr")
+  #status=$?
+
+  if [[ $status -eq 2 ]]
+  then
+    $CUDA_EXE -O -i min_no_ntr.in -o min_no_ntr.out -x min_no_ntr.nc -r min_no_ntr.rst7 -inf min_no_ntr.info -p $TOPO -c min_ntr_l.rst7
+  touch "min_no_ntr_successful.tmp"
+  fi
+}
+
+############################################################
+# Strip WAT, Na+, Cl- from rst7
+############################################################
+
+function process_rst7 () {
+  local LIG=$1
+  
+  cat > remove_solvent.in <<EOF
+parm ../../../topo/${LIG}_solv_com.parm7
+trajin min_no_ntr.rst7
+strip :WAT,Na+,Cl-
+trajout min_no_ntr_noWAT.rst7
+EOF
+}
+
+############################################################
+# Perform MM/PBGSA calculations
+############################################################
+
+function run_rescoring () {
+  local LIG=$1
+  local INPUT_FILE=$2
+  local TRAJECTORY=$3
+  local COM_TOP=$4
+  local REC_TOP=$5
+  local LIG_TOP=$6
+  local THREADS=$7
+  local EXE=$8
+  local TYPE=$9
+  local RESCORING_PATH=${10}
+
+  echo "##############"
+  echo " Rescoring"
+  echo "##############"
+  
+  cd ${RESCORING_PATH}
+
+  ${EXE} -O \
+  -i $INPUT_FILE \
+  -cp ../../../topo/$COM_TOP \
+  -rp ../../../topo/$REC_TOP \
+  -lp ../../../topo/$LIG_TOP \
+  -y ${TRAJECTORY} \
+  -o ${LIG}_${TYPE}.data \
+  -eo ${LIG}_${TYPE}_frame.data
+  }
+
+############################################################
+# Process Ligand
+############################################################
+# Testing to run with gnu parallel
+
+function run_ligand_rescoring() {
+  lig=$1
+  rep=$2
+  WDPATH=$3
+  RECEPTOR=$4
+
+
+  # Topology and coord file
+  TOPO="${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${lig}/topo"
+  RESCORING_PATH="${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${lig}/setupMD/rep${rep}/mmpbsa_rescoring"
+  
+ 
+  run_rescoring $lig \
+  "mm_gbsa.in" \
+  "min_no_ntr_noWAT.rst7" \
+  "${lig}_vac_com.parm7" \
+  "${lig}_vac_rec.parm7" \
+  "${lig}_vac_lig.parm7" \
+  1 \
+  MMPBSA.py \
+  "mmgbsa" \
+  ${RESCORING_PATH}
+
+}
+
+############################################################
+# Main
+############################################################
+
+#Ruta de la carpeta del script (donde se encuentra este script)
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Ruta de la carpeta de trabajo. Es SCRIPT_PATH / WD_PATH
+WDPATH=($(realpath $WD_PATH))
+
+# Receptor
+RECEPTOR_PDB=($(basename "${WDPATH}/receptor/"*.pdb))
+if [[ ${#RECEPTOR_PDB[@]} -eq 0 ]]
+then
+    echo "Empty receptor folder."
+    echo "Exiting."
+    exit 1
+fi
+
+RECEPTOR=($(sed "s/.pdb//g" <<< "${RECEPTOR_PDB[*]}"))
+
+# Ligandos analizados
+LIGANDS_MOL2=("${WDPATH}/ligands/"*.mol2)
+
+if [[ ${#LIGANDS_MOL2[@]} -eq 0 ]]
+then
+    echo "Empty ligands folder."
+    echo "Exiting."
+    exit 1
+fi
+
+LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
+
+CUDA_EXE=${AMBERHOME}/bin/pmemd.cuda
+
+echo
+echo "###########################"
+echo "# Receptor is ${RECEPTOR} #"
+echo "###########################"
+echo 
+
+################################
+# Testing GNU parallel
+################################
+
+# Export the function to be used by parallel
+export -f run_ligand_rescoring
+export -f run_rescoring
+
+# Generate a list of all jobs
+jobs=()
+for LIG in "${LIGANDS[@]}"
+  do
+    LIG=$(basename "${LIG}")
+    # For each replica-ligand combination, prepare the job for parallel execution
+    jobs+=("$LIG 1 ${WDPATH} ${RECEPTOR}")
+  done
+
+
+
+# Run
+printf "%s\n" "${jobs[@]}" | parallel -j $NUM_CORES --colsep ' ' run_ligand_rescoring {1} {2} {3} {4}
+
