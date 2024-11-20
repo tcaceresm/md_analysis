263b6b3c410f949bbaa790271277c2d9bb82b1ac
tcaceresm <caceres.tomas99@gmail.com>
2024-11-20 12:45:37 -0300

    updated file

--- scripts/setupMD/run_MD.sh
+++ scripts/setupMD/run_MD.sh
@@ -134,86 +134,112 @@ echo "###########################"
 echo "# Receptor is ${RECEPTOR} #"
 echo "###########################"
 
-for rep in $(seq $REPLICAS_START $REPLICAS_END) # Repetitions
+for rep in $(seq ${REPLICAS_START} ${REPLICAS_END}) # Repetitions
   do
     echo "#########################"
     echo "# Doing replica ${rep} !#"
     echo "#########################"
-    echo 
+    echo
 
-    if [[ $EQUI -eq 1 ]]
+    EQUI_protocol=("min_ntr_h" "min_ntr_l" "md_nvt_ntr" "md_npt_ntr" "npt_equil_1" "npt_equil_2" "npt_equil_3" "npt_equil_4" "npt_equil_5" "npt_equil_6")
+
+
+    if [[ ${ONLY_PROTEIN_MD} -eq 1 ]]
       then
 
-      echo "##########################"
-      echo "# Starting equilibration #"
-      echo "##########################"
+        echo 
+        echo " # Protein-only! #"
+        echo 
 
-      EQUI_protocol=("min_ntr_h" "min_ntr_l" "md_nvt_ntr" "md_npt_ntr" "npt_equil_1" "npt_equil_2" "npt_equil_3" "npt_equil_4" "npt_equil_5" "npt_equil_6")
 
-      if [[ $ONLY_PROTEIN_MD -eq 1 ]]
+        if [[ ${EQUI} -eq 1 ]]
         then
 
-          echo 
-          echo " # Protein-only! #"
-          echo 
+          echo "#########################################"
+          echo "# Starting equilibration - Protein Only #"
+          echo "#########################################"
 
           # Topology and coord file
           CRD=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/topo/${RECEPTOR}_solv
           TOPO=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/topo/${RECEPTOR}_solv
           EQUI_PATH=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${rep}/equi/
 
-          cd $EQUI_PATH
+          cd ${EQUI_PATH}
 
-          OLD=$CRD
+          OLD=${CRD}
           RESTRAINED=1
 
           for STEP in "${EQUI_protocol[@]}"
             do
-              if [[ "$STEP" == "npt_equil_1" ]] #change directory
+              if [[ "${STEP}" == "npt_equil_1" ]] #change directory
               then
-                cd $EQUI_PATH/npt 
+                cd ${EQUI_PATH}/npt 
                 OLD="../${OLD}"             
               fi
-              if [[ "$STEP" == "npt_equil_6" ]]
+              if [[ "${STEP}" == "npt_equil_6" ]]
               then
                 RESTRAINED=0
               fi
-              NEW=$STEP
-              run_MD $OLD $NEW $TOPO $CRD $RESTRAINED
-              OLD=$NEW
+              NEW=${STEP}
+              run_MD ${OLD} ${NEW} ${TOPO} ${CRD} ${RESTRAINED}
+              OLD=${NEW}
             done
-      fi
-      
-      if [[ $PROT_LIG_MD -eq 1 ]]
+        fi
+
+        if [[ ${PROD} -eq 1 ]]
         then
-
-          echo 
-          echo " # Protein-Ligand! #"
-          echo 
-
-          LIGANDS_MOL2=("${WDPATH}/ligands/"*.mol2)
-          LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
           
-          for LIG in "${LIGANDS[@]}"
-            do
+          echo "######################################"
+          echo "# Starting production - Protein Only #"
+          echo "######################################"
+          
+          # Topology and coord file
+          TOPO=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/topo/${RECEPTOR}_solv
+          PROD_PATH=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${rep}/prod/npt
+          OLD=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${rep}/equi/npt/npt_equil_6
 
-            LIG=$(basename "${LIG}")
-            echo "  Doing for ${LIG}"
+          cd ${PROD_PATH}
+          run_MD ${OLD} md_prod ${TOPO} "" 0
 
+        fi
+
+
+    fi
+
+      
+    if [[ ${PROT_LIG_MD} -eq 1 ]]
+      then
+
+        echo 
+        echo " # Protein-Ligand! #"
+        echo 
+
+        LIGANDS_MOL2=("${WDPATH}/ligands/"*.mol2)
+        LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
+        
+        for LIG in "${LIGANDS[@]}"
+          do
+
+          LIG=$(basename "${LIG}")
+
+          echo "  Doing for ${LIG}"
+
+          if [[ ${EQUI} -eq 1 ]]
+          then
             # Topology and coord file
             CRD=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/topo/${LIG}_solv_com
             TOPO=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/topo/${LIG}_solv_com
             EQUI_PATH=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/setupMD/rep${rep}/equi/     
 
-            OLD=$CRD
+            OLD=${CRD}
 
-            cd $EQUI_PATH
+            cd ${EQUI_PATH}
 
             for STEP in ${EQUI_protocol[@]}
               do
                 if [[ "$STEP" == "npt_equil_1" ]] #change directory
                 then
-                  cd $EQUI_PATH/npt     
+                  cd ${EQUI_PATH}/npt     
                   OLD="../${OLD}" 
                 fi
 
@@ -223,60 +249,31 @@ for rep in $(seq $REPLICAS_START $REPLICAS_END) # Repetitions
                 else
                   RESTRAINED=1
                 fi               
-                NEW=$STEP
-                run_MD $OLD $NEW $TOPO $CRD $RESTRAINED
-                OLD=$NEW
+                NEW=${STEP}
+                run_MD ${OLD} ${NEW} ${TOPO} ${CRD} ${RESTRAINED}
+                OLD=${NEW}
               done
-            done
-      fi
+          fi
 
+
+          
+          if [[ ${PROD} -eq 1 ]]
+            then
+              
+              echo "##############################"
+              echo "# Starting production ${LIG} #"
+              echo "##############################"
+  
+              # Topology and coord file
+              TOPO=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/topo/${LIG}_solv_com
+              PROD_PATH=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/setupMD/rep${rep}/prod/npt
+              OLD=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/setupMD/rep${rep}/equi/npt/npt_equil_6
+
+              cd ${PROD_PATH}
+
+              run_MD ${OLD} md_prod ${TOPO} "" 0
+          fi
+        done
     fi
 
-    if [[ $PROD -eq 1 ]]
-    then
-
-      echo "##########################"
-      echo "# Starting production    #"
-      echo "##########################"
-
-      if [[ $ONLY_PROTEIN_MD -eq 1 ]]
-        then
-          
-          echo 
-          echo " # Protein-only! #"
-          echo 
-          
-          # Topology and coord file
-          TOPO=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/topo/${RECEPTOR}_solv
-          PROD_PATH=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${rep}/prod/npt
-          OLD=${WDPATH}/MD/${RECEPTOR}/onlyProteinMD/rep${rep}/equi/npt/npt_equil_6
-
-          cd $PROD_PATH
-          run_MD $OLD md_prod $TOPO "" 0
-
-      fi
-      
-      if [[ $PROT_LIG_MD -eq 1 ]]
-        then
-          
-          echo 
-          echo " # Protein-Ligand! #"
-          echo
-
-          declare -a LIGANDS_MOL2=($(ls ${WDPATH}/ligands/))
-          declare -a LIGANDS=($(sed "s/.mol2//g" <<< "${LIGANDS_MOL2[*]}"))
-      
-          for LIG in "${LIGANDS[@]}"
-          do
-            # Topology and coord file
-            TOPO=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/topo/${LIG}_solv_com
-            PROD_PATH=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/setupMD/rep${rep}/prod/npt
-            OLD=${WDPATH}/MD/${RECEPTOR}/proteinLigandMD/${LIG}/setupMD/rep${rep}/equi/npt/npt_equil_6
-
-            cd $PROD_PATH
-            run_MD $OLD md_prod $TOPO "" 0
-
-          done
-      fi
-    fi
   done
